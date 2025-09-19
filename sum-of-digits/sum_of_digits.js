e6ba0538eef9a5bbe66cb4283de8ecd4c64d300a
jenifferjojo <jenifferjtc@gmail.com>
2025-09-19 17:12:08 +0530

    Update sum_of_digits.js
    adding a block of the same program lets me handle cases from 1-99.

--- sum-of-digits/sum_of_digits.js
+++ sum-of-digits/sum_of_digits.js
@@ -1,7 +1,12 @@
-const input = 13;
+const input = 11;
 let remainingDigits = input;
 let digitalSum = 0;
 let digit = 0;
+
+digit = remainingDigits%10;
+digitalSum = digitalSum + digit;
+remainingDigits = remainingDigits - digit;
+remainingDigits = remainingDigits/10;
 
 digit = remainingDigits%10;
 digitalSum = digitalSum + digit;
