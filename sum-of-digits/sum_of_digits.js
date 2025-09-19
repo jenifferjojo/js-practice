362d2034bcaef4d318ce5a390f34ec6685ad2730
jenifferjojo <jenifferjtc@gmail.com>
2025-09-19 17:16:12 +0530

    Update sum_of_digits.js
    Put the block of code into a while loop, which holds true as long as the value of remainingDigits is greater than 0.

--- sum-of-digits/sum_of_digits.js
+++ sum-of-digits/sum_of_digits.js
@@ -1,16 +1,13 @@
-const input = 11;
-let remainingDigits = input;
+const input = 999;
 let digitalSum = 0;
 let digit = 0;
+let remainingDigits = input;
 
-digit = remainingDigits%10;
-digitalSum = digitalSum + digit;
-remainingDigits = remainingDigits - digit;
-remainingDigits = remainingDigits/10;
-
-digit = remainingDigits%10;
-digitalSum = digitalSum + digit;
-remainingDigits = remainingDigits - digit;
-remainingDigits = remainingDigits/10;
+while(remainingDigits > 0){
+  digit = remainingDigits%10;
+  digitalSum = digitalSum + digit;
+  remainingDigits = remainingDigits - digit;
+  remainingDigits = remainingDigits/10;
+}
 
 console.log("The Digital Sum of",input,"is",digitalSum);
