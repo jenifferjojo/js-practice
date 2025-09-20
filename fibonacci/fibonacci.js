ea66ab8996e325d934a2e5e0df616a90efcdb9c3
jenifferjojo <jenifferjtc@gmail.com>
2025-09-20 11:06:46 +0530

    Update fibonacci.js
    After printing the current term, made it hold the value of the next term and changed the value of nextTerm to hold the value of the next of the next term.

--- fibonacci/fibonacci.js
+++ fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const numberOfTerms = 2;
-const nextTermValue = 1;
-const currentTermValue = 0;
+const numberOfTerms = 1;
+let nextTermValue = 1;
+let currentTermValue = 0;
 
 console.log(currentTermValue);
+nextTermValue = nextTermValue + currentTermValue;
+currentTermValue = nextTermValue - currentTermValue;
