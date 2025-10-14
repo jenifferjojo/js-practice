3ee879e170cb3a6a6771a345ce7a61be656cf9bd
jenifferjojo <jenifferjtc@gmail.com>
2025-10-14 15:08:23 +0530

    Update find_path.js

--- find_path.js
+++ find_path.js
@@ -9,20 +9,29 @@ function character(element) {
   return element[0] === 1 ? PATH : BOMB;
 }
 
-function generateRow(rowItems) {
+function printRow(rowItems) {
   const line = [];
-  for (let index = 0; index < rowItems.length; index++) {
+  line[0] = rowItems[0];
+  for (let index = 1; index < rowItems.length; index++) {
     line[index] = character(rowItems[index]);
   }  
 
   return line.join("");
 }
 
-function generateMap(map) {
+function printMap(map) {
   console.clear();
   const grid = [];
+  const line = [];
+
   for (let index = 0; index < map.length; index++) {
-    grid[index] = generateRow(map[index]);
+    line[index] = map[0][index];
+  }
+
+  grid.push(line.join(" "));
+
+  for (let index = 1; index < map.length; index++) {
+    grid[index] = printRow(map[index]);
   }
 
   return grid.join("\n");
@@ -43,13 +52,13 @@ function reveal(map, coordinates) {
 }
 
 function revealMap(mapRow) {
-  for (let index = 0; index < mapRow.length; index++) {
+  for (let index = 1; index < mapRow.length; index++) {
     mapRow[index][1] = true;
   }
 }
 
 function checkRow(rowItems) {
-  let index = 0;
+  let index = 1;
   while (index < rowItems.length) {
     if (rowItems[index][0] === 1 && !rowItems[index][1]) return false;
     index++;
@@ -61,39 +70,87 @@ function checkRow(rowItems) {
 function checkWon(map) {
   let index = map.length - 1;
 
-  while (index >= 0) {
+  while (index >= 1) {
     if(!checkRow(map[index])) {
       return false;
     }
     index--;
   }
 
-  return index === -1;
+  return index === 0;
 }
 
-function generate(n) {
-  const map = [[[" "], [1], [2], [3], [4], [5], [6]], 
-               [[1], [1, false], [1, false], [1, false], [0, false], [0, false], [0, false]],
-               [[2], [0, false], [0, false], [1, false], [0, false], [0, false], [0, false]],
-               [[3], [0, false], [0, false], [1, false], [1, false], [0, false], [0, false]],
-               [[4], [0, false], [0, false], [0, false], [1, false], [0, false], [0, false]],
-               [[5], [0, false], [0, false], [0, false], [1, false], [0, false], [0, false]],
-               [[6], [1, false], [0, false], [0, false], [0, false], [0, false], [0, false]]];
+function generatePath(map) {
+  console.log(map);
+  let start = (Math.floor(Math.random() * 100) % (map[1].length - 1)) + 1;
+
+  map[1][start][0] = 1;
+  map[2][start][0] = 1;
+
+  let directions = [-1, 0, 1];
+
+  let index = 2;
+  while (index !== map.length - 1) {
+    const direction = (Math.floor(Math.random() * 10)) % directions.length;
+    
+    console.log(direction, index, start);
+    if (directions[direction] === 0) {
+      map[++index][start][0] = 1;
+    }
+
+    if (directions[direction] === -1 && start !== 1) {
+      map[index][--start][0] = 1;
+      // map[++index][start][0] = 1;
+      directions = [-1, 0];
+    }
+
+    if (directions[direction] === 1 && start !== map[index].length - 1) {
+      map[index][++start][0] = 1;
+      // map[++index][start][0] = 1;
+      directions = [0, 1];
+    }
+  }
+
   return map;
 }
 
+function generateRow(row, n) {
+  for (let index = 1; index < n; index++) {
+    row[index] = [];
+    row[index][0] = 0;
+    row[index][1] = false;
+  }
+}
+
+function generateMap(n) {
+  const map = [];
+
+  map[0] = [];
+  for (let index = 0; index < n; index++) {
+    map[0].push(index);
+  }
+
+  for (let index = 1; index < n; index++) {
+    map[index] = [];
+    map[index][0] = index;
+    generateRow(map[index], n);
+  }
+
+  return generatePath(map);;
+}
+
 function main() {
-  const map = [[[0, false], [1, false], [0, false], [0, false], [0, false], [0, false]], 
-               [[0, false], [1, false], [1, false], [1, false], [0, false], [0, false]],
-               [[0, false], [0, false], [0, false], [1, false], [0, false], [0, false]],
-               [[0, false], [0, false], [0, false], [1, false], [1, false], [0, false]],
-               [[0, false], [0, false], [0, false], [0, false], [1, false], [0, false]],
-               [[0, false], [0, false], [0, false], [0, false], [1, false], [0, false]]];
+  // const map = [[[0, false], [1, false], [0, false], [0, false], [0, false], [0, false]], 
+  //              [[0, false], [1, false], [1, false], [1, false], [0, false], [0, false]],
+  //              [[0, false], [0, false], [0, false], [1, false], [0, false], [0, false]],
+  //              [[0, false], [0, false], [0, false], [1, false], [1, false], [0, false]],
+  //              [[0, false], [0, false], [0, false], [0, false], [1, false], [0, false]],
+  //              [[0, false], [0, false], [0, false], [0, false], [1, false], [0, false]]];
 
 
-  // const map = generate();
+  const map = generateMap(10);
 
-  console.log(generateMap(map));
+  console.log(printMap(map));
   let guessRemaining = 5;
 
   while (guessRemaining > 0 && !checkWon(map)) {
@@ -105,17 +162,17 @@ function main() {
       guessRemaining = guessRemaining - 1;
     }
 
-    console.log(generateMap(map));
+    console.log(printMap(map));
   }
 
   const message = guessRemaining === 0 ? "You Lost!" : "You WON";
 
   if (message === "You Lost!") {
-    for (let index = 0; index < map.length; index++) {
+    for (let index = 1; index < map.length; index++) {
       revealMap(map[index]);
     }
 
-    console.log(generateMap(map));
+    console.log(printMap(map));
   }
   console.log(message);
 
